8f02baf91d335377c1ede642f71c37b1d2aceb64
helendotr <97937374+helendotr@users.noreply.github.com>
2023-03-28 08:47:23 +0300

    Pievienoju nepieciešamās funkcijas

---
+++ script.js
@@ -0,0 +1,25 @@
+const plusins = document.querySelector('.js-plus');
+const inputins = document.querySelector('.js-input');
+
+const Add = (cipars1, cipars2) => {
+return cipars1 + cipars2
+}
+
+const Subtract = (cipars1, cipars2) => {
+    return cipars1 - cipars2
+}
+
+const Multiply = (cipars1, cipars2) => {
+    return cipars1 * cipars2
+}
+
+const Divide = (cipars1, cipars2) => {
+    return cipars1 / cipars2
+}
+
+plusins.addEventListener('click', () => {
+    console.log("Mani nospieda")
+}) 
+console.log(Add(8,7))
+
+console.log(inputins)
