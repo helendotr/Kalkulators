4feca33cedee3723cc831e9680131e8f2fa9ad12
helendotr <97937374+helendotr@users.noreply.github.com>
2023-04-18 08:42:58 +0300

    Pievienojām pogu funkcionalitāti, sākām style.css

--- script.js
+++ script.js
@@ -6,9 +6,6 @@ const inputins1 = document.querySelector('.js-input1');
 const inputins2 = document.querySelector('.js-input2');
 const answer = document.querySelector('.js-result');
 
-let firstValue = inputins1.value
-let secondValue = inputins2.value
-
 const Add = (cipars1, cipars2) => {
     return cipars1 + cipars2
 }
@@ -25,6 +22,25 @@ const Divide = (cipars1, cipars2) => {
     return cipars1 / cipars2
 }
 
+const clear = () => {
+    inputins1.value = ""
+    inputins2.value = ""
+}
+
 plusins.addEventListener('click', () => {
     answer.textContent = Add(parseInt(inputins1.value),parseInt(inputins2.value))
+
+    clear()
+})
+
+minus.addEventListener('click', () => {
+    answer.textContent = Subtract(parseInt(inputins1.value),parseInt(inputins2.value))
+
+    clear()
+})
+
+multiply.addEventListener('click', () => {
+    answer.textContent = Multiply(parseInt(inputins1.value),parseInt(inputins2.value))
+
+    clear()
 })
